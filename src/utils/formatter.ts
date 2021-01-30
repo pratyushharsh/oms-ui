13a20d390dde83e560760bfd600b1e3941af6017
Pratyush Harsh <pratyushharsh2015@gmail.com>
2021-01-30 15:00:08 +0530

    Shipping Address Change

---
+++ src/utils/formatter.ts
@@ -0,0 +1,34 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+const shortMonths = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC'
+]
+
+export function dateFormatter(data: string | Date) {
+    var date = new Date(data);
+    return `${date.getDay()}/${shortMonths[date.getMonth()]}/${date.getFullYear()}`
+}
